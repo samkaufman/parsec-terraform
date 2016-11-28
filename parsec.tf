2bcd1540b10f6f5ce8215c965a21c3859212f8c9
Sam Kaufman <emrysk@gmail.com>
2016-11-28 14:55:19 -0500

    Switched to spots

--- parsec.tf
+++ parsec.tf
@@ -16,23 +16,23 @@ variable "aws_region" {
   type = "string"
 }
 
-
 # IAM user for reading from the deploy bucket
 
 resource "aws_iam_user" "parsec_instance" {
-    name = "parsec-instance-user"
+  name = "parsec-instance-user"
 }
 
 resource "aws_iam_access_key" "parsec_instance_user" {
-    user = "${aws_iam_user.parsec_instance.name}"
+  user = "${aws_iam_user.parsec_instance.name}"
 }
 
 resource "aws_iam_user_policy" "parsec_instance_user_perms" {
-    # TODO: Restrict to read-only
-    name = "parsec-instance-user-policy"
-    user = "${aws_iam_user.parsec_instance.name}"
-    depends_on = ["aws_s3_bucket.deploybucket"]
-    policy= <<EOF
+  # TODO: Restrict to read-only
+  name       = "parsec-instance-user-policy"
+  user       = "${aws_iam_user.parsec_instance.name}"
+  depends_on = ["aws_s3_bucket.deploybucket"]
+
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -49,25 +49,24 @@ resource "aws_iam_user_policy" "parsec_instance_user_perms" {
 EOF
 }
 
-
 # CloudWatch alarms to bring instance down if we forget
 
 resource "aws_cloudwatch_metric_alarm" "idle_instance" {
-    alarm_name = "parsec-idle-alarm"
-    comparison_operator = "LessThanThreshold"
-    evaluation_periods = "10"
-    metric_name = "CPUUtilization"
-    namespace = "AWS/EC2"
-    period = "180"
-    statistic = "Maximum"
-    threshold = "5"
-    alarm_description = "Alarm waits for idle Parsec instances"
-    alarm_actions = ["arn:aws:automate:${var.aws_region}:ec2:terminate"]
-    dimensions {
-        InstanceId = "${aws_instance.parsec.id}"
-    }
-}
+  alarm_name          = "parsec-idle-alarm"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "10"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "180"
+  statistic           = "Maximum"
+  threshold           = "5"
+  alarm_description   = "Alarm waits for idle Parsec instances"
+  alarm_actions       = ["arn:aws:automate:${var.aws_region}:ec2:terminate"]
 
+  dimensions {
+    InstanceId = "${aws_spot_instance_request.parsec.spot_instance_id}"
+  }
+}
 
 # A few non-standard DLLs we want to send
 
@@ -77,128 +76,133 @@ resource "aws_s3_bucket" "deploybucket" {
 }
 
 resource "aws_s3_bucket_object" "xinputdll" {
-  bucket   = "${aws_s3_bucket.deploybucket.bucket}"
-  key      = "System32/XInput9_1_0.dll"
-  source   = "System32/XInput9_1_0.dll"
-  etag     = "${md5(file("System32/XInput9_1_0.dll"))}"
+  bucket = "${aws_s3_bucket.deploybucket.bucket}"
+  key    = "System32/XInput9_1_0.dll"
+  source = "System32/XInput9_1_0.dll"
+  etag   = "${md5(file("System32/XInput9_1_0.dll"))}"
 }
 
 resource "aws_s3_bucket_object" "vcredist64" {
-  bucket   = "${aws_s3_bucket.deploybucket.bucket}"
-  key      = "Redist/vcredist_x64.exe"
-  source   = "Redist/vcredist_x64.exe"
-  etag     = "${md5(file("Redist/vcredist_x64.exe"))}"
+  bucket = "${aws_s3_bucket.deploybucket.bucket}"
+  key    = "Redist/vcredist_x64.exe"
+  source = "Redist/vcredist_x64.exe"
+  etag   = "${md5(file("Redist/vcredist_x64.exe"))}"
 }
 
 resource "aws_s3_bucket_object" "vcredist86" {
-  bucket   = "${aws_s3_bucket.deploybucket.bucket}"
-  key      = "Redist/vcredist_x86.exe"
-  source   = "Redist/vcredist_x86.exe"
-  etag     = "${md5(file("Redist/vcredist_x86.exe"))}"
+  bucket = "${aws_s3_bucket.deploybucket.bucket}"
+  key    = "Redist/vcredist_x86.exe"
+  source = "Redist/vcredist_x86.exe"
+  etag   = "${md5(file("Redist/vcredist_x86.exe"))}"
 }
-
 
 # Core Formation
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region     = "${var.aws_region}"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret}"
 }
 
 data "aws_ami" "parsec" {
   most_recent = true
+
   filter {
-    name = "name"
+    name   = "name"
     values = ["parsec-ws2012-1"]
   }
 }
 
 resource "aws_security_group" "parsec" {
-  name = "parsec"
+  name        = "parsec"
   description = "Allow inbound Parsec+VNC traffic and all outbound."
 
   ingress {
-      from_port = 8000
-      to_port = 8004
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8000
+    to_port     = 8004
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-      from_port = 8000
-      to_port = 8004
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8000
+    to_port     = 8004
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-      from_port = 8000
-      to_port = 8004
-      protocol = "udp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8000
+    to_port     = 8004
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-      from_port = 5900
-      to_port = 5901
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 5900
+    to_port     = 5901
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 data "template_file" "user_data" {
-    # TODO: Pass in S3 filekeys as vars (which introduces deps.)
-    template = "${file("user_data.tmpl")}"
-    vars {
-        authcode = "${var.parsec_authcode}"
-        deploy_bucket = "${aws_s3_bucket.deploybucket.bucket}"
-        iam_key = "${aws_iam_access_key.parsec_instance_user.id}"
-        iam_secret = "${aws_iam_access_key.parsec_instance_user.secret}"
-        reg_user_root = "HKEY_USERS\\S-1-5-21-2194300291-1390441514-1065349922-500"
-        vcredist86_key = "${aws_s3_bucket_object.vcredist86.key}"
-        vcredist64_key = "${aws_s3_bucket_object.vcredist64.key}"
-        xinputdll_key = "${aws_s3_bucket_object.xinputdll.key}"
-    }
+  # TODO: Pass in S3 filekeys as vars (which introduces deps.)
+  template = "${file("user_data.tmpl")}"
+
+  vars {
+    authcode       = "${var.parsec_authcode}"
+    deploy_bucket  = "${aws_s3_bucket.deploybucket.bucket}"
+    iam_key        = "${aws_iam_access_key.parsec_instance_user.id}"
+    iam_secret     = "${aws_iam_access_key.parsec_instance_user.secret}"
+    reg_user_root  = "HKEY_USERS\\S-1-5-21-2194300291-1390441514-1065349922-500"
+    vcredist86_key = "${aws_s3_bucket_object.vcredist86.key}"
+    vcredist64_key = "${aws_s3_bucket_object.vcredist64.key}"
+    xinputdll_key  = "${aws_s3_bucket_object.xinputdll.key}"
+  }
 }
 
 resource "aws_ebs_volume" "parsec_game_drive" {
-    size = 100
-    type = "gp2"
-    availability_zone = "us-east-1b"
-    lifecycle {
-      prevent_destroy = true
-    }
-    tags {
-      Name = "ParsecGameDrive"
-    }
+  size              = 100
+  type              = "gp2"
+  availability_zone = "us-east-1b"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    Name = "ParsecGameDrive"
+  }
 }
 
 resource "aws_volume_attachment" "game_drive_att" {
-    device_name = "xvdg"
-    volume_id = "${aws_ebs_volume.parsec_game_drive.id}"
-    instance_id = "${aws_instance.parsec.id}"
+  device_name = "xvdg"
+  volume_id   = "${aws_ebs_volume.parsec_game_drive.id}"
+  instance_id = "${aws_spot_instance_request.parsec.spot_instance_id}"
 }
 
-resource "aws_instance" "parsec" {
-    # spot_price = "0.7"
-    # wait_for_fulfillment = true
-    # spot_type = "one-time"
-    ami = "${data.aws_ami.parsec.id}"
-    instance_type = "g2.2xlarge"
-    availability_zone = "${aws_ebs_volume.parsec_game_drive.availability_zone}"
-    instance_initiated_shutdown_behavior = "terminate"
+/*resource "aws_instance" "parsec" {*/
+resource "aws_spot_instance_request" "parsec" {
+  spot_price           = "0.8"
+  wait_for_fulfillment = true
+  spot_type            = "one-time"
+  ami                  = "${data.aws_ami.parsec.id}"
 
-    tags {
-      Name = "ParsecServer"
-    }
+  instance_type                        = "g2.2xlarge"
+  availability_zone                    = "${aws_ebs_volume.parsec_game_drive.availability_zone}"
+  instance_initiated_shutdown_behavior = "terminate"
 
-    root_block_device {
-      volume_size = 30
-    }
+  tags {
+    Name = "ParsecServer"
+  }
 
-    user_data = "${data.template_file.user_data.rendered}"
+  root_block_device {
+    volume_size = 30
+  }
 
-    security_groups = ["${aws_security_group.parsec.name}"]
-    associate_public_ip_address = true
+  user_data = "${data.template_file.user_data.rendered}"
+
+  security_groups             = ["${aws_security_group.parsec.name}"]
+  associate_public_ip_address = true
 }
